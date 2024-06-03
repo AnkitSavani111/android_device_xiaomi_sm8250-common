0962522eb50ed73ad8083863d291ce3aa05e55f5
HDzungx <hdzungx@gmail.com>
2024-06-03 23:01:49 +0530

    sm8250-common: Add support for MIUI camera

--- libinit/libvariant_headers/include/libvariant.h
+++ libinit/libvariant_headers/include/libvariant.h
@@ -16,6 +16,7 @@ struct variant_info {
     std::string brand;
     std::string device;
     std::string marketname;
+    std::string mod_device;
     std::string model;
     std::string build_fingerprint;
 
