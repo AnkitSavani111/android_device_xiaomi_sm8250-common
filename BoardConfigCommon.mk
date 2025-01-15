687e9f24a5dc194703c41122caed03d93c73beea
AnkitSavani111 <ankitmsavani12345@gmail.com>
2025-01-15 22:09:48 +0530

    sm8250-common: Update blobs from munch HyperOS OS1.0.8.0.ULMMIXM
    Signed-off-by: AnkitSavani111 <ankitmsavani12345@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -199,7 +199,7 @@ SOONG_CONFIG_XIAOMI_KONA_PARTITION_SCHEME := a
 endif
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2024-09-01
+VENDOR_SECURITY_PATCH := 2024-12-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
