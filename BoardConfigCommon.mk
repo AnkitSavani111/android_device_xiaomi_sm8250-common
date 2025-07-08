9c64d7d7ceec40098de6ac635836189292c24b29
basamaryan <basam.aryan@gmail.com>
2025-07-08 15:51:28 -0400

    sm8250-common: Override kernel BPF version
    Change-Id: I3c581f3120b6258b3b45020c5ba12f6bf5b0ee27

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -158,12 +158,12 @@ TARGET_BOARD_PLATFORM := kona
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 ifneq ($(TARGET_IS_TABLET),true)
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor_phone.prop
 endif
-TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 
 # Recovery
 ifeq ($(TARGET_IS_VAB),true)
